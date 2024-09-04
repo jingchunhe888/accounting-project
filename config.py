ad8ed92bb579ac7b2196b4fa4d52f4c3a3c37d8b
jingchunhe888 <132230624+jingchunhe888@users.noreply.github.com>
2024-09-04 00:29:29 -0400

    Create config.py
    config for bank names, column names etc.

---
+++ config.py
@@ -0,0 +1,19 @@
+file_path_history_config = '/Users/jinhe/Documents/Event 365 LLC.xlsx'
+
+table_settings = {
+    "vertical_strategy": "lines",
+    "horizontal_strategy": "text",
+    "snap_y_tolerance": 4,
+    "intersection_x_tolerance": 3,
+}
+
+file_path_config = '/Users/jinhe/Documents/M&T Bank/Files'
+
+# '/Users/jinhe/Documents/Different Bank Types/Chase Business Select Checking.pdf'
+base_year_config = 20
+
+columns_config = ["Date", "Memo", "Deposits and Other Credits(+)","Withdrawals & Other Debits (-)"]
+description_config = 'Description'
+all_columns_config = ['Date','Description','Account','Memo','Deposits and Other Credits(+)','Withdrawals & Other Debits (-)']
+memo_config = 'Memo'
+bank_config = ['MANUFACTURERS AND TRADERS TRUST COMPANY','JPMORGAN CHASE BANK']
