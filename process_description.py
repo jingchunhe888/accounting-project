15288b1b7405b466200a965f3a9b718093f7d9d6
jingchunhe888 <132230624+jingchunhe888@users.noreply.github.com>
2024-09-09 13:38:42 -0400

    Update process_description.py
    removes different receipts and dates to minimize unique values

--- process_description.py
+++ process_description.py
@@ -96,7 +96,7 @@ def process_description(description):
         # else:
         #     description = part_after_to
 
-    pattern_state_card = re.compile(r'(New York)? (([A-Z]){2})? Card \d{4}')
+    pattern_state_card = re.compile(r'(New York)? ?(([A-Z]){2})? Card \d{4}$')
     description = re.sub(pattern_state_card,'',description)
     # match_pattern_state_card = re.search(pattern_state_card, description)
     # if match_pattern_state_card:
@@ -106,7 +106,9 @@ def process_description(description):
     #     description = ' '.join(description.split())
     # else:
     #     pass
-    description = re.sub(r'(\d{2}/\d{2})', '', description)
+
+    description = re.sub(r'(\d{1,2}[\/-]\d{1,2})([\/-]\d{2,4})?','',description)
+    # description = re.sub(r'(\d{2}(\/|-)\d{2})', '', description)
 
 
     #do not delete everything beforehand 
@@ -131,20 +133,26 @@ def process_description(description):
     #         description = re.sub(r'\S*\*', '', description)
 
     #chase bank characteristics 
-    pattern = r'Orig CO Name:(.*?) Orig ID:'
-    match = re.search(pattern, description)
+    pattern_orig = r'Orig CO Name:(.*?) Orig ID:'
+    match = re.search(pattern_orig, description)
     if match:
         description = match.group(1).strip()
     description = re.sub(r'PPD ID:','', description)
     description = re.sub(r'CCD ID:','', description)
     description = re.sub(r'Web ID:','', description)
-    description = re.sub(r'^(Recurring )?Card Purchase (With Pin)?','',description)
+    description = re.sub(r'^(Recurring )?Card Purchase (With Pin)? ?(Return)?','',description)
     description = re.sub(r'^Refund of', '', description)
     
     pattern_adj_fee = r'Foreign Exch Rt ADJ Fee.*'
     match = re.search(pattern_adj_fee,description)
     if match: 
         description = 'Foreign Exch Rt ADJ Fee'
+        return description
+    
+    pattern_adj_fee = r'Insufficient Funds Fee.*'
+    match = re.search(pattern_adj_fee,description)
+    if match: 
+        description = 'Insufficient Funds Fee'
         return description
     # description = re.sub(r'CP WEB PYMT P','',description)
     # description = re.sub(r'In [EDI ]?P[A]?YM[E]?NTS','',description)
@@ -159,48 +167,86 @@ def process_description(description):
     # description = re.sub(r'\.Com\*','.Com ',description, flags=re.IGNORECASE)
     
     description = re.sub(r'\.\.\.', ' ', description)
-    #special characters, not & not () not /
-    description = re.sub(r'[!"#\$%\'*+,.:;<=>?@[\\\]\^_`{|}~]',' ',description)
+
+    
+    #space dependent, normalize spacing beforehand
     description = re.sub(r' {2,}',' ',description)
     word_counts = len(description.split())
     if word_counts < 2: 
         pass
     else: 
-        description = re.sub(r'\d{7,}','',description)
+        #replacing digits first before deleting receipts
         description = re.sub(r'\(.*\)', '', description)
-
     word_counts = len(description.split())
+    print(description)
     if word_counts <= 1:
         pass
     else:
+        description = re.sub(r'\*',' ',description)
         #pattern_receipt = re.compile(r'[a-zA-Z\d]{6,}(-[a-zA-Z\d]{1,})*')
-        pattern_receipt = re.compile(r'(?=.*?\d)(?=.*?[a-zA-Z])[a-zA-Z\d]{4,}')
+        pattern_receipt = re.compile(r'(?=\w*\d)(?=\w*[a-zA-Z\-!"#\$%\'*+,.:;<=>?@[\\\]\^_`{|}~])[a-zA-Z0-9-\-!"#\$%\'*+,.:;<=>?@[\\\]\^_`{|}~]{4,}')
+        words = description.split()
+        for i in range(1,len(words)):
+            if re.search(pattern_receipt,words[i]):
+                if re.search(r'[a-zA-Z]', words[i]):
+                    if re.search(r'\d', words[i]):
+                        words[i] = re.sub(pattern_receipt,'',words[i])
+        description = ' '.join(words)
+
+#space creating
+#special characters, not & not () not / not *(usually separating text and receipt), replace after check receipt after first word
+    description = re.sub(r'[!"#\$%\'*+,.:;<=>?@[\\\]\^_`{|}~]',' ',description)
+
         # match_receipt = re.search(pattern_receipt,description)
-        match_receipt_iter = re.finditer(pattern_receipt, description)
-        # description = re.sub(pattern_receipt,'',description)
-        for match in match_receipt_iter:
-            match = match.group()
-            # print(match)
-            if re.search(r'[a-zA-Z]', match):
-                if re.search(r'\d', match):
-                    description = re.sub(match, '', description)
+    #     match_receipt_iter = re.finditer(pattern_receipt, description)
+    #     # description = re.sub(pattern_receipt,'',description)
+    #     for match in match_receipt_iter:
+    #         match = match.group()
+    #         # print(match)
+    #         if re.search(r'[a-zA-Z]', match):
+    #             if re.search(r'\d', match):
+    #                 description = re.sub(match, '', description)
     description = re.sub(r'-',' ',description)
     description = re.sub(r' {2,}',' ',description)
+    description = re.sub(r'\d{7,}','',description)
 
-    word_counts = len(description.split())
-    if word_counts <=1:
-        pass
-    else:
-        description = re.sub(r'\b(?=\w*\d)(?=\w*[a-zA-Z\-!"#\$%\'*+,.:;<=>?@[\\\]\^_`{|}~])[a-zA-Z0-9-\-!"#\$%\'*+,.:;<=>?@[\\\]\^_`{|}~]{4,}\b','',description)
+    # word_counts = len(description.split())
+    # if word_counts <=1:
+    #     pass
+    # else:
+    #     #might not anchor at the end, 
+        #receipt pattern with special charactersw: (?=.*?\d)(?=.*?[a-zA-Z])[a-zA-Z\d]{4,}
+        # description = re.sub(r'\b(?=\w*\d)(?=\w*[a-zA-Z\-!"#\$%\'*+,.:;<=>?@[\\\]\^_`{|}~])[a-zA-Z0-9-\-!"#\$%\'*+,.:;<=>?@[\\\]\^_`{|}~]{4,}\b$','',description)
         # description = re.sub(r'\d{5,9}', '', description)
         # description = re.sub(r'Transfer To Chk | Transfer From Chk', ' Transfer To/From', description)
         # description = re.sub(r'\S*#\S*', '', description)
     description = description.strip()
-    description = re.sub(r'(Assoc|Corp.*|Inc.*|Ltd|Mfg|Mfrs|LLC.*)$','',description,flags=re.IGNORECASE)
-    # pattern_amazon = re.compile(r'Amzn Mktp US Amzn\.Com/Bill')
-    # match_pattern_amazon = re.search(pattern_amazon, description)
-    # if match_pattern_amazon:
-    #     description = 'Amzn Mktp Amzn.Com/Bill'
-    #     return description
-    # print('this is description' + description)
+    description = re.sub(r'(\bAssoc\b|\bCorp\b.*|\bInc\b.*|\bLtd\b|\bMfg\b|\bMfrs\b|\bLLC\b.*)$','',description,flags=re.IGNORECASE)
+    description = re.sub(r' {2,}',' ',description)
     return description.strip()
+
+
+# de = process_description('Monthly Service Fee Charged On 05-29-2020')
+# print(de)
+
+# # Load the Excel file and specify the sheet and column(s)
+# df = pd.read_excel('/Users/jinhe/Documents/Event 365 LLC.xlsx', sheet_name='2023')
+
+# # Initialize an empty list to store processed descriptions
+# description = []
+
+# # Iterate over each value in the "Description" column
+# for value in df['Description']:
+#     if pd.isna(value) or value == '':  # Check if the value is NaN or an empty string
+#         description.append('')  # Append an empty string for NaN or empty values
+#     else:
+#         new = process_description(value)  # Apply your processing function to non-empty values
+#         description.append(new)  # Append the processed value to the list
+
+# # Convert the 'description' list into a DataFrame with a column named "Description"
+# # description_df = pd.DataFrame(description, columns=['Description'])
+# # description_df.to_clipboard(index=False)
+# # Print the resulting DataFrame
+# # print(description_df)
+
+
