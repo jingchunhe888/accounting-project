b0558c51b07a4b828673a9f60c1cab42e5e0295e
jingchunhe888 <132230624+jingchunhe888@users.noreply.github.com>
2024-09-09 13:09:47 -0400

    Update main.py
    added all df

--- main.py
+++ main.py
@@ -4,13 +4,15 @@
 #fourth fill in the values and get empty values pycharm
 
 import pandas as pd
+import random
 import pdfplumber
 from config import *
 import os
 import datetime
 import re
-from process_df import *
+from process_df import process_description
 from chase import *
+from ai import edit
 
 def format_date(date):
     pattern_date = re.compile(r'^\d{1,2}\/\d{1,2}\/\d{2,4}$')
@@ -28,6 +30,8 @@ def format_date(date):
 # Function to extract text and other information from a page
 def extract_original(page):
     text = page.extract_text()
+    # print('text!')
+    # print(text)
     count = text.lower().count('page')
     account_activity = text.lower().count('account activity')
     h = page.height
@@ -49,6 +53,7 @@ def print_df(table):
 def get_rows(page):
     text, count, h, w , account_activity = extract_original(page)
     if count == 0: 
+        print('no crop found in the pdf and skips page')
         return None
     else: 
         while True: 
@@ -99,31 +104,43 @@ def is_valid_date(date_str):
     if bool(pattern_date.match(date_str)) == True:
         return bool(pattern_date.match(date_str))
     else: 
-        print(f'wrong date {date_str}')
+        # print(f'wrong date {date_str}')
         return bool(pattern_date.match(date_str))
 
 
 
 # Apply function to filter DataFrame
 
-
+#return df
 def driver(file_path, bank):
+    print('driver running')
     if os.path.isdir(file_path):
         all_df = []  # Flat list to store all DataFrames from all files
         for filename in os.listdir(file_path):
+            # print('inside driver loop')
             full_path = os.path.join(file_path, filename)
-            if os.path.isfile(full_path):  # Check if it's a file
+            if full_path.startswith('.') or not full_path.lower().endswith('.pdf'):
+                continue
+            if os.path.isfile(full_path):  # Check if it's a file')
                 if bank == bank_config[0]:
                     try: 
+                        # print('equals to first bank_config')
                         combined_df = bank_main(full_path)
+                        # print(combined_df)
                         all_df.append(combined_df)
                     except Exception as e: 
-                        print(f'Problem with {full_path}: {e}')
-                if bank == bank_config[1]:
-                    print('to be determined 1')
+                        print(f'Problem with bank_main {full_path}: {e}')
+                elif bank == bank_config[1] or bank== bank_config[2]:
+                    try: 
+                        combined_df = chase(full_path)
+                        all_df.append(combined_df)
+                    except Exception as e: 
+                        print(f'ID 2 Problem with {full_path}: {e}')
         # Concatenate all DataFrames from all files
         if bank == bank_config[0]:
+            print('driver out of loop running')
             if all_df:  # Ensure that all_df is not empty
+                print('all_df exists')
                 final_combined_df = pd.concat(all_df, ignore_index=True)
                 final_combined_df = final_combined_df[~(final_combined_df == '').all(axis=1)]
                 # print(final_combined_df.shape)
@@ -143,20 +160,28 @@ def driver(file_path, bank):
                 
                 # print(final_combined_df.to_string())
                 final_combined_df.reset_index(drop=True, inplace=True)
-                final_combined_df.to_clipboard(index = True)
+                return final_combined_df
+                #update entire df instead of Memo column
+                # copy = final_combined_df[memo_config]
+                # copy.to_clipboard(index = True)
             else:
                 print("No tables found in any of the files.")
                 
-        if bank == bank_config[1]:
-            print('to be determined 2')
+        elif bank == bank_config[1] or bank== bank_config[2]:
+            print('second option combine')
+            final_combined_df = pd.concat(all_df, ignore_index=True)
+            return final_combined_df
+            #all_df['Memo'].to_clipboard(index = True)
 
     #add extra section for individual files
     else:
         if os.path.isfile(file_path):  # Check if it's a file
             if bank == bank_config[0]:
                 m_and_t_file(file_path)
-            if bank == bank_config[1]:
-                print('to be determined')
+            if bank == bank_config[1] or bank== bank_config[2]:
+                print('is file chase bank')
+                df = chase(file_path) #returns df
+                return df
             
 
 def m_and_t_file(file_path):
@@ -183,11 +208,11 @@ def m_and_t_file(file_path):
             
             # print(final_combined_df.to_string())
             final_combined_df.reset_index(drop=True, inplace=True)
-            final_combined_df.to_clipboard(index = True)
+            # final_combined_df.to_clipboard(index = True)
         else:
             print("No tables found in any of the files.")
     except Exception as e: 
-        print(f'Problem with {file_path}: {e}') #instead of full_path
+        print(f'M_and_T with {file_path}: {e}') #instead of full_path
 
         
 
@@ -195,6 +220,7 @@ def get_bank(file_path, bank):
     pdf = pdfplumber.open(file_path)
     for page in pdf.pages:
         text = page.extract_text()
+        print(text)
         bank_count = text.upper().count(bank)
         if bank_count > 0: 
             return bank
@@ -203,24 +229,46 @@ def get_bank(file_path, bank):
 
 def main(file_path):
     if os.path.isdir(file_path):
+        files_same_bank = []
         for filename in os.listdir(file_path):
             full_path = os.path.join(file_path, filename)
+            if full_path.startswith('.') or not full_path.lower().endswith('.pdf'):
+                continue
             if os.path.isfile(full_path):  # Check if it's a file
-                check_bank(full_path, file_path)
+                print('checks passed')
+                is_bank = check_bank(full_path, file_path)
+                files_same_bank.append(is_bank)
+
+        if is_bank and all(x==files_same_bank[0] for x in files_same_bank) if files_same_bank else False:
+            df = driver(file_path, is_bank)
+            return df
     else:
+        if file_path.startswith('.') or not file_path.lower().endswith('.pdf'):
+            pass
         if os.path.isfile(file_path):
-            check_bank(file_path, file_path)  # Check if it's a file
+            # print('this is it 1')
+            is_bank = check_bank(file_path, file_path)  # Check if it's a file
+        if is_bank:
+            df = driver(file_path,is_bank)
+            return df
 
 def check_bank(file_full_path, file_path):
     try: 
         for bank in bank_config: 
+            if file_full_path.startswith('.') or not file_full_path.lower().endswith('.pdf'):
+                # print('filepath name')
+                # print(file_full_path)
+                continue
             is_bank = get_bank(file_full_path, bank)
+            # print('i am here')
+            # print('this is bank')
+            print(is_bank)
             if is_bank: 
-                driver(file_path, bank)
-            else: 
-                print("We cannot process your bank yet.")
+                return is_bank
+        if not is_bank: 
+            print("We cannot process your bank yet.")
     except Exception as e: 
-        print(f'1 Problem with {file_full_path}: {e}')
+        print(f'Problem with check_bank {file_full_path}: {e}')
 
     # try: 
     #     is_bank_1 = get_bank(file_full_path, bank_config)
@@ -231,5 +279,67 @@ def check_bank(file_full_path, file_path):
     #         # chase(file_full_path)
     # except Exception as e: 
     #     print(f'1 Problem with {file_full_path}: {e}')
-   
-main(file_path_config)
+
+# update uncomment   
+df = main(file_path_config)
+# print(df.to_string())
+# print(df[memo_config].to_string())
+df[description_config] = df.apply(
+    lambda row: process_description(row[memo_config]) if pd.isna(row[description_config]) else row[description_config],
+    axis=1
+) 
+
+
+# print(df.to_string())
+df = df.drop_duplicates(subset=description_config,keep='first',ignore_index=True)
+dfcopy = df[[description_config,memo_config,deposit_config,withdrawal_config]]
+dfcopy.to_clipboard()
+print(dfcopy.to_string())
+
+# df_description = pd.Series(df[description_config].unique())
+# df_description.to_clipboard()
+# print(df_description.to_string())
+
+
+#uncomment block for AI 
+
+# def edit(text):
+#     count = random.randint(1,10000)
+#     text=count
+#     return text
+
+df_ai = dfcopy.head(5).copy()
+df_ai['AI Suggested'] = None
+# print(df_ai.to_string())
+df_ai['AI Suggested'] = df_ai[description_config].apply(edit)
+df_ai = df_ai.drop_duplicates(subset='AI Suggested',keep='first',ignore_index=True)
+df_ai = df_ai[['AI Suggested',description_config,memo_config,deposit_config,withdrawal_config]]
+print(df_ai.to_string())
+
+
+df_ai[account_config] = 'Sales'
+
+dict_fill_acct = df_ai.set_index('AI Suggested')[account_config].to_dict()
+dict_fill_des = df_ai.set_index(description_config)['AI Suggested'].to_dict()
+
+print(dict_fill_acct)
+
+
+df_ai[description_config]=df_ai[description_config].replace(dict_fill_des)
+df_ai[account_config] = df_ai[description_config].map(dict_fill_acct)
+df_ai = df_ai[[description_config,account_config,memo_config,deposit_config,withdrawal_config]]
+print(df_ai.to_string())
+
+
+
+# df_ai_vendor = df_description.apply(edit)
+# df_ai_vendor = pd.DataFrame(df_ai_vendor, columns=['AI Suggested'])
+# print(df_ai_vendor.to_string())
+
+
+# df = df[[description_config,memo_config]]
+# print(df.to_string())
+# update delete afterwards 
+# bank = 'MANUFACTURERS AND TRADERS TRUST COMPANY'
+# driver(file_path_config, bank)
+
