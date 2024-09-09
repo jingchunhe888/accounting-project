af002a71d115c8cd94fba6840daee4445e33ad47
jingchunhe888 <132230624+jingchunhe888@users.noreply.github.com>
2024-09-09 13:08:45 -0400

    Update config.py

--- config.py
+++ config.py
@@ -11,9 +11,13 @@ file_path_config = '/Users/jinhe/Documents/M&T Bank/Files'
 
 # '/Users/jinhe/Documents/Different Bank Types/Chase Business Select Checking.pdf'
 base_year_config = 20
-
+deposit_config = "Deposits and Other Credits(+)"
+withdrawal_config = "Withdrawals & Other Debits (-)"
+account_config = 'Account'
 columns_config = ["Date", "Memo", "Deposits and Other Credits(+)","Withdrawals & Other Debits (-)"]
 description_config = 'Description'
 all_columns_config = ['Date','Description','Account','Memo','Deposits and Other Credits(+)','Withdrawals & Other Debits (-)']
 memo_config = 'Memo'
-bank_config = ['MANUFACTURERS AND TRADERS TRUST COMPANY','JPMORGAN CHASE BANK']
+bank_config = ['MANUFACTURERS AND TRADERS TRUST COMPANY','JPMORGAN CHASE BANK','WWW.CHASE.COM']
+
+
